7f2198ee05d520c4d10a35bb6a3fddeeda05e4e8
Ismail Sunni <imajimatika@gmail.com>
2018-06-27 11:26:39 +0700

    Add model for ContextCollection and ContextGroup.

--- django_project/geocontext/admin.py
+++ django_project/geocontext/admin.py
@@ -6,6 +6,8 @@ from django.contrib.gis.admin import OSMGeoAdmin
 
 from geocontext.models.context_service_registry import ContextServiceRegistry
 from geocontext.models.context_cache import ContextCache
+from geocontext.models.context_group import ContextGroup
+from geocontext.models.context_collection import ContextCollection
 
 
 class ContextServiceRegistryAdmin(admin.ModelAdmin):
@@ -18,5 +20,17 @@ class ContextCacheAdmin(OSMGeoAdmin):
     list_display = ('name', 'service_registry', 'value', 'expired_time')
 
 
+class ContextGroupAdmin(admin.ModelAdmin):
+    """Context Group admin model."""
+    list_display = ('key', 'name')
+
+
+class ContextCollectionAdmin(admin.ModelAdmin):
+    """Context Collection admin model."""
+    list_display = ('key', 'name')
+
+
 admin.site.register(ContextServiceRegistry, ContextServiceRegistryAdmin)
 admin.site.register(ContextCache, ContextCacheAdmin)
+admin.site.register(ContextGroup, ContextGroupAdmin)
+admin.site.register(ContextCollection, ContextCollectionAdmin)
---
+++ django_project/geocontext/models/collection_groups.py
@@ -0,0 +1,20 @@
+# coding=utf-8
+"""Model for CollectionGroups"""
+
+from django.db import models
+
+from geocontext.models.context_collection import ContextCollection
+from geocontext.models.context_group import ContextGroup
+
+
+class CollectionGroups(models.Model):
+    """Collection Groups"""
+
+    context_group = models.ForeignKey(ContextGroup, on_delete=models.CASCADE)
+    context_collection = models.ForeignKey(
+        ContextCollection, on_delete=models.CASCADE)
+    order = models.IntegerField(
+        verbose_name='Order',
+        null=False,
+        blank=True
+    )
