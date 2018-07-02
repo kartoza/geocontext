74fa886a748f1c5002015425fbe6efeb37cfad5f
Ismail Sunni <imajimatika@gmail.com>
2018-07-02 16:05:19 +0700

    Always do back up before importing geocontext data.

--- django_project/base/management/commands/delete_data.py
+++ django_project/base/management/commands/delete_data.py
@@ -3,12 +3,10 @@
 
 from django.core.management.base import BaseCommand
 import logging
-
-from geocontext.models.context_service_registry import ContextServiceRegistry
-from geocontext.models.context_group import ContextGroup
-from geocontext.models.context_collection import ContextCollection
+from .utilities import delete_data
 
 logger = logging.getLogger(__name__)
+
 
 
 class Command(BaseCommand):
@@ -19,26 +17,4 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         print('Deleting GeoContext Data...')
 
-        print('Before delete process...')
-        print('Number of CSR %s' % ContextServiceRegistry.objects.count())
-        print('Number of Context Group %s' % ContextGroup.objects.count())
-        print('Number of Context Collection %s' %
-              ContextCollection.objects.count())
-
-        context_service_registries = ContextServiceRegistry.objects.all()
-        for context_service_registry in context_service_registries:
-            context_service_registry.delete()
-
-        context_groups = ContextGroup.objects.all()
-        for context_group in context_groups:
-            context_group.delete()
-
-        context_collections = ContextCollection.objects.all()
-        for context_collection in context_collections:
-            context_collection.delete()
-
-        print('After delete process...')
-        print('Number of CSR %s' % ContextServiceRegistry.objects.count())
-        print('Number of Context Group %s' % ContextGroup.objects.count())
-        print('Number of Context Collection %s' %
-              ContextCollection.objects.count())
+        delete_data()
---
+++ django_project/base/management/commands/utilities.py
@@ -0,0 +1,153 @@
+# coding=utf-8
+"""Utilities for commands."""
+
+import json
+
+from geocontext.models.context_service_registry import ContextServiceRegistry
+from geocontext.models.context_group import ContextGroup
+from geocontext.models.context_collection import ContextCollection
+
+from geocontext.models.context_group_services import ContextGroupServices
+from geocontext.models.collection_groups import CollectionGroups
+
+from geocontext.serializers.context_service_registry import (
+    ContextServiceRegistrySerializer)
+from geocontext.serializers.context_group import ContextGroupSerializer
+from geocontext.serializers.context_collection import (
+    ContextCollectionSerializer)
+
+
+def export_data(file_path):
+    """Export context service data to file_path as json file.
+
+    :param file_path: Path to json file.
+    :type file_path: str
+    """
+    # Context Service Registries
+    context_service_registries = ContextServiceRegistry.objects.all()
+    csr_serializer = ContextServiceRegistrySerializer(
+        context_service_registries, many=True)
+
+    # Context Groups
+    context_groups = ContextGroup.objects.all()
+    context_group_serializer = ContextGroupSerializer(
+        context_groups, many=True)
+
+    # Context Collection
+    context_collections = ContextCollection.objects.all()
+    context_collection_serializer = ContextCollectionSerializer(
+        context_collections, many=True)
+
+    # Aggregate Data
+    data = {
+        'context_service_registry': csr_serializer.data,
+        'context_group': context_group_serializer.data,
+        'context_collection': context_collection_serializer.data
+    }
+
+    with open(file_path, 'w') as outfile:
+        json.dump(
+            data,
+            outfile,
+            indent=4,
+            ensure_ascii=False)
+
+
+def import_data(file_path):
+    """Import context service data from file_path.
+
+    :param file_path: Path to json file.
+    :type file_path: str
+    """
+    # Read json file
+    with open(file_path) as f:
+        data = json.load(f)
+
+    # Load Context Service Registries
+    context_service_registries = data['context_service_registry']
+    for csr_data in context_service_registries:
+        service_registry, created = ContextServiceRegistry.objects. \
+            get_or_create(key=csr_data['key'])
+        # Change to load from dictionary
+        for k, v in csr_data.items():
+            setattr(service_registry, k, v)
+        service_registry.save()
+
+    # Load Context Groups
+    context_groups = data['context_group']
+    for context_group_data in context_groups:
+        context_group, created = ContextGroup.objects. \
+            get_or_create(key=context_group_data['key'])
+        # Change to load from dictionary
+        for k, v in context_group_data.items():
+            if k == 'context_service_registry_keys':
+                context_service_registry_keys = v
+                i = 0
+                for csr_key in context_service_registry_keys:
+                    context_service_registry = \
+                        ContextServiceRegistry.objects.get(key=csr_key)
+                    context_group_service = ContextGroupServices(
+                        context_group=context_group,
+                        context_service_registry=context_service_registry,
+                        order=i
+                    )
+                    context_group_service.save()
+                    i += 1
+            setattr(context_group, k, v)
+        context_group.save()
+
+    # Load Context Collections
+    context_collections = data['context_collection']
+    for context_collection_data in context_collections:
+        context_collection, created = ContextCollection.objects. \
+            get_or_create(key=context_collection_data['key'])
+        # Change to load from dictionary
+        for k, v in context_collection_data.items():
+            if k == 'context_group_keys':
+                context_group_keys = v
+                i = 0
+                for context_group_key in context_group_keys:
+                    context_group = ContextGroup.objects.get(
+                        key=context_group_key)
+                    collection_group = CollectionGroups(
+                        context_collection=context_collection,
+                        context_group=context_group,
+                        order=i
+                    )
+                    collection_group.save()
+                    i += 1
+            setattr(context_collection, k, v)
+        context_collection.save()
+
+    print('After import data process...')
+    print('Number of CSR %s' % ContextServiceRegistry.objects.count())
+    print('Number of Context Group %s' % ContextGroup.objects.count())
+    print('Number of Context Collection %s' %
+          ContextCollection.objects.count())
+
+
+def delete_data():
+    """Delete geocontext data utilities method."""
+    print('Before delete process...')
+    print('Number of CSR %s' % ContextServiceRegistry.objects.count())
+    print('Number of Context Group %s' % ContextGroup.objects.count())
+    print('Number of Context Collection %s' %
+          ContextCollection.objects.count())
+
+    context_service_registries = ContextServiceRegistry.objects.all()
+    for context_service_registry in context_service_registries:
+        context_service_registry.delete()
+
+    context_groups = ContextGroup.objects.all()
+    for context_group in context_groups:
+        context_group.delete()
+
+    context_collections = ContextCollection.objects.all()
+    for context_collection in context_collections:
+        context_collection.delete()
+
+    print('After delete process...')
+    print('Number of CSR %s' % ContextServiceRegistry.objects.count())
+    print('Number of Context Group %s' % ContextGroup.objects.count())
+    print('Number of Context Collection %s' %
+          ContextCollection.objects.count())
