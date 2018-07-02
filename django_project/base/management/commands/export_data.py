43fd3af1fcd7e6b6cd28641002b4444c10364b0d
Ismail Sunni <imajimatika@gmail.com>
2018-07-02 16:05:19 +0700

    Always do back up before importing geocontext data.

--- django_project/base/management/commands/export_data.py
+++ django_project/base/management/commands/export_data.py
@@ -2,22 +2,12 @@
 """Management command to export GeoContext data to JSON file."""
 
 import os
-import json
 
 from django.core.management.base import BaseCommand
 import logging
 
-from geocontext.models.context_service_registry import ContextServiceRegistry
-from geocontext.models.context_group import ContextGroup
-from geocontext.models.context_collection import ContextCollection
+from .utilities import export_data
 
-from geocontext.serializers.context_service_registry import (
-    ContextServiceRegistrySerializer
-)
-from geocontext.serializers.context_group import ContextGroupSerializer
-from geocontext.serializers.context_collection import (
-    ContextCollectionSerializer
-)
 
 logger = logging.getLogger(__name__)
 
@@ -34,32 +24,6 @@ class Command(BaseCommand):
             'geocontext.json'
         )
 
-        # Context Service Registries
-        context_service_registries = ContextServiceRegistry.objects.all()
-        csr_serializer = ContextServiceRegistrySerializer(
-            context_service_registries, many=True)
+        export_data(geocontext_file)
 
-        # Context Groups
-        context_groups = ContextGroup.objects.all()
-        context_group_serializer = ContextGroupSerializer(
-            context_groups, many=True)
-
-        # Context Collection
-        context_collections = ContextCollection.objects.all()
-        context_collection_serializer = ContextCollectionSerializer(
-            context_collections, many=True)
-
-        # Aggregate Data
-        data = {
-            'context_service_registry': csr_serializer.data,
-            'context_group': context_group_serializer.data,
-            'context_collection': context_collection_serializer.data
-        }
-
-        with open(geocontext_file, 'w') as outfile:
-            json.dump(
-                data,
-                outfile,
-                indent=4,
-                ensure_ascii=False)
         print('Export GeoContext data finished...')
