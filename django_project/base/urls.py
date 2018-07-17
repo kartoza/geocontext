c55f0fd72f61b0d02f3624f7fce90d83ef9d68f3
Ismail Sunni <imajimatika@gmail.com>
2018-07-17 11:20:28 +0700

    Add prefix for API view.

--- django_project/base/urls.py
+++ django_project/base/urls.py
@@ -2,8 +2,14 @@
 
 from django.urls import path
 from django.views.generic import TemplateView
+from django.conf.urls import url, include
+
 from geocontext.urls import urlpatterns as geocontext_urls
+from geocontext.urls import urlpatterns_api as geocontext_url_api
+
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name="landing_page.html")),
-] + geocontext_urls
+    url('^api/v1/', include(geocontext_url_api)),
+    url('', include(geocontext_urls))
+]
