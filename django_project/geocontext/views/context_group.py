7c31d53c866b84c4b147509e5d2a8abadf740e33
Ismail Sunni <imajimatika@gmail.com>
2018-07-19 15:15:13 +0700

    Add detail view for model.

--- django_project/geocontext/views/context_group.py
+++ django_project/geocontext/views/context_group.py
@@ -1,7 +1,7 @@
 # coding=utf-8
 """View for Context Group."""
 
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from geocontext.models.context_group import ContextGroup
 
 
@@ -12,3 +12,12 @@ class ContextGroupListView(ListView):
     template_name = 'geocontext/context_group_list.html'
     context_object_name = 'context_group_list'
     paginate_by = 10
+
+
+class ContextGroupDetailView(DetailView):
+    """Detail view for Context Group."""
+
+    model = ContextGroup
+    template_name = 'geocontext/context_group_detail.html'
+    context_object_name = 'context_group'
+    slug_field = 'key'
