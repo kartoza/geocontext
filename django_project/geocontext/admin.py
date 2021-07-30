7f5dd259f600b0f5d60fe6eb2859aec98911b192
Faneva Andriamiadantsoa <fanevanjanahary@gmail.com>
2021-07-30 12:01:29 +0300

    Show associated groups in the layer

--- django_project/geocontext/admin.py
+++ django_project/geocontext/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.contrib.gis.admin import OSMGeoAdmin
 
+from django.urls import reverse
+from django.utils.safestring import mark_safe
 from geocontext.models.cache import Cache
 from geocontext.models.collection import Collection
 from geocontext.models.collection_groups import CollectionGroups
@@ -13,7 +15,7 @@ class GroupServicesInLine(admin.TabularInline):
     """Inline Admin for GroupServices"""
     model = GroupServices
     sortable_field_name = 'order'
-    ordering = ('order', )
+    ordering = ('order',)
     extra = 0
 
 
@@ -27,7 +29,18 @@ class CollectionGroupsInLine(admin.TabularInline):
 
 class ServiceAdmin(admin.ModelAdmin):
     """Service admin model."""
-    list_display = ('key', 'name', 'query_type', 'url')
+    list_display = ('key', 'name', 'query_type', 'url', 'groups')
+    search_fields = ('key', 'name')
+
+    def groups(self, service):
+        groups = Group.objects.filter(services=service)
+        html = ''
+        for group in groups:
+            html += '<li><a href="{}">{}</a></li>'.format(
+                reverse("admin:geocontext_group_change", args=(group.pk,)),
+                group.key
+            )
+        return mark_safe('<ul style="list-style-type:none">{}</ul>'.format(html))
 
 
 class CacheAdmin(OSMGeoAdmin):
